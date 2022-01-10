73617b369941d9b94247b5f24ec3f15daa6a84fe
richardjim <richardiyevhobu@gmail.com>
2022-01-10 12:00:12 +0200

    New

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+      doloremque, voluptatibus nulla consectetur quia aliquid nisi. Animi
+      deleniti nostrum quam ipsum sunt! Voluptates porro temporibus dignissimos
+      saepe laboriosam! Ullam, culpa eos aut sequi deleniti a quisquam provident
+      accusantium fugiat. Praesentium tenetur adipisci delectus aspernatur
+      suscipit, unde commodi consequatur perferendis ipsum. Ducimus ut
+      exercitationem qui illum ex itaque placeat eos, doloribus nisi culpa
+      delectus sed quisquam dolor aspernatur neque? Autem maiores, esse quia ab
+      temporibus ipsa necessitatibus illum dignissimos, facere nobis molestias
+      veritatis nesciunt odio nisi quos architecto sit, aspernatur nihil sunt
+      eos? Mollitia, nobis atque. Dolorum ipsum nihil eligendi maiores?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+      doloremque, voluptatibus nulla consectetur quia aliquid nisi. Animi
+      deleniti nostrum quam ipsum sunt! Voluptates porro temporibus dignissimos
+      saepe laboriosam! Ullam, culpa eos aut sequi deleniti a quisquam provident
+      accusantium fugiat. Praesentium tenetur adipisci delectus aspernatur
+      suscipit, unde commodi consequatur perferendis ipsum. Ducimus ut
+      exercitationem qui illum ex itaque placeat eos, doloribus nisi culpa
+      delectus sed quisquam dolor aspernatur neque? Autem maiores, esse quia ab
+      temporibus ipsa necessitatibus illum dignissimos, facere nobis molestias
+      veritatis nesciunt odio nisi quos architecto sit, aspernatur nihil sunt
+      eos? Mollitia, nobis atque. Dolorum ipsum nihil eligendi maiores?
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
---
+++ vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    publicPath: '/vue1js/'
+}
