393ca9d2084ce473bf910d8c9b34b213c028a5a7
richardjim <richardiyevhobu@gmail.com>
2022-01-11 17:41:15 +0200

    debug

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+// module.exports = {
+//     publicPath: '/vue1js/'
+// }
 module.exports = {
-    publicPath: '/vue1js/'
-}
+    publicPath: process.env.NODE_ENV === "production" ? "/vue1js/" : "/",
+  };
